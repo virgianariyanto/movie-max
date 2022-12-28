9c3e10dafe141e6198bf8ac4309a9f25f91ea939
Virgian Jaya Ariyanto <Virgian.Ariyanto@job-tomori.com>
2022-12-28 17:18:55 +0700

    menambahkan beberapa fitur

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Input from "./components/Input"
 import Card from "./components/Card"
+import { IconSearch } from "@tabler/icons";
 import { useEffect, useState } from "react"
 import "./App.css"
 
@@ -8,7 +9,7 @@ const API_URL = "http://www.omdbapi.com?apikey=3be35780";
 
 function App() {
     const [movies, setMovies] = useState([]);
-    
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovie = async (title) => {
         const respone = await fetch(`${API_URL}&s=${title}`);
@@ -17,13 +18,20 @@ function App() {
         setMovies(data.Search);
       }
       useEffect(() => {
-        searchMovie('Love');
+        searchMovie('cinta');
       }, []);
   
   return (
     <>
       <Header />
-      <Input />
+        <h1 className="w-full text-center text-slate-800 text-3xl font-bold py-10">Search Movie</h1>
+          <section className="w-full mt-10 flex flex-wrap justify-center">
+            <div className="w-1/2 flex items-center gap-x-1">
+              <Input setSearchTerm={setSearchTerm} />
+              <button id="search-button" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded text-sm px-5 py-2.5 focus:outline-none" onClick={() => searchMovie(searchTerm) }><IconSearch />
+              </button>
+            </div>
+          </section>
       <div className="flex justify-center my-10">
           {
             movies?.length > 0
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,10 @@
 import './../App.css'
-import { IconSearch } from "@tabler/icons";
 
-export default function Input() {
+export default function Input({searchTerm, setSearchTerm}) {
+
     return (
-        <section className="w-full mt-10 flex flex-wrap justify-center">
-            <h1 className="w-full text-center text-slate-800 text-3xl font-bold py-10">Search Movie</h1>
-            <div className="w-1/2 flex items-center gap-x-1">
-                <input type="text" id="search-input" className="px-2 py-3 text-slate-600 relative rounded text-sm border-0 shadow focus:ring-2 focus:border-blue-600 focus:outline-none w-5/6" placeholder="Title" />
-                <button id="search-button" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded text-sm px-5 py-2.5 focus:outline-none"><IconSearch /></button>
-            </div>
-        </section>
+        <>
+            <input type="text" id="search-input" className="px-2 py-3 text-slate-600 relative rounded text-sm border-0 shadow focus:ring-2 focus:border-blue-600 focus:outline-none w-5/6" placeholder="Title" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        </>
     )
 }
